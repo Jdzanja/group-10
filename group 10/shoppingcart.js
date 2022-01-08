22f825da1d96eb8958c9276cbcc2e4d7fd68d0a2
Joseph Dzanja <bit-021-19@must.ac.mw>
2022-01-08 22:15:35 +0200

    commit message

--- group 10/shoppingcart.js
+++
@@ -1,7 +0,0 @@
-var cart_item = document.getElementsByClassName("cart_item");
-
-for (var i = 0; i < cart_item.length; i++) {
-  cart_item[i].childNodes[1].addEventListener("click", function() {
-    this.parentElement.classList.toggle("listVisible");
-  })
-}
